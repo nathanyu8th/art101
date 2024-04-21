0332aee571da3ad669314b5f3e1b4cc528f223f7
nathanyu8th <nathanyu8th@gmail.com>
2024-04-21 15:53:04 -0700

    lab4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,10 +1,44 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Checkers
+//             An implementation of how to create the game checkers.
+// Author: Nathan Yu <nyu16@ucsc.edu>
+// Date:4/18/24
 
 // Constants
 
 // Functions
+
+//Pseudocode
+/*
+Task 2:
+Checkers
+  Print the board before every turn
+    Make changes to the board regarding player moves
+  Accept User Input
+    Reveal possible moves for the piece that the player chooses
+  Execute User Input
+    Move pieces after user confirms their move
+    If the piece can make any more moves, let the player move that piece again.
+    If not, end the players turn
+  Create Possible Player Moves
+    Make the simple checkers piece able to only move forward
+    If the piece reaches the end of the board, promote it
+    The piece can now move forwards and backwards
+  Program AI for the Computer
+    Set up a neural network for the AI
+    Feed it hundreds of test data from previous checkers games found online.
+    Create 3 different difficulties
+      Make different settings for each bot
+  Make the win condition
+    If a player loses all their pieces, they lose and the other player wins
+    If neither player can move, then it’s a draw
+  Create UI
+    Make the possible moves bright to make it noticeable to the player
+    When the game ends, show the player’s result
+      Show 2 buttons
+        Play Again
+        Go Back to Title
+*/
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
