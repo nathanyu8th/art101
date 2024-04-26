b4b7455e395d52c5791022a22530af4892a17bbf
nathanyu8th <nathanyu8th@gmail.com>
2024-04-25 17:22:11 -0700

    added lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,26 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Creating Variables with Javascript
+// Author: Nathan Yu
+// Date: 4/25/24
 
 // Constants
 
+make = "Ford";
+model = "Taurus";
+color = "Rusty";
+year = 2004;
+ownIt = true;
+
+//calculate
+age = 2024 - year;
+
 // Functions
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
+document.writeln("Owned: " + ownIt + "<br>");
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
