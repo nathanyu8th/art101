d6ac4c0007b97f3412b411bd3d8fa1fb76f1fa14
nathanyu8th <nathanyu8th@gmail.com>
2024-05-02 16:11:35 -0700

    lab6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,11 +1,24 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab6 Arrays and Objects
+// Author: Nathan Yu
+// Date: 4/28/24
 
 // Constants
+var myTransport = ["walking", "bus", "uber"];
 
+var myMainRide = {make: "Ford",
+  model: "Taurus",
+  color: "Rusty",
+  year: 1995,
+  age: function(){
+    return 2024 - this.year;
+  }
+}
 // Functions
 
+document.writeln("Getting around: " + myTransport + "<br>");
+
+document.writeln("My Main Ride: <pre>", 
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
